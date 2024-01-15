ecf4c5b73c1cbb11521e6aed5db3a385d03a8982
rodrigollo <rodrigogollo.nh@gmail.com>
2024-01-15 08:13:40 -0300

    feat: add getUsers fetch from backend api example

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,31 @@
 import './App.css'
-
+import { useEffect, useState } from 'react'
 function App() {
+
+  const [users, setUsers] = useState<[] | null>([]);
+
+  useEffect(() => {
+    setUsers([]);
+    async function getUsers(){
+      const url = "http://localhost:3000/api/v1/users"
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+      setUsers(data);
+    }
+    getUsers()
+
+  }, []);
 
   return (
     <>
       <div>
         Hello world
+        {users && 
+          users.map(({id, username}: any) => {
+            return <p key={id}>{username}</p>
+          })
+        }
       </div>
     </>
   )
