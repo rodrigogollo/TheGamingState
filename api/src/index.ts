69f59c2ebcaa06a8a05ff1d391546a8c570b1911
rodrigollo <rodrigogollo.nh@gmail.com>
2024-01-15 08:18:31 -0300

    feat: add cors and fixed database relations (User, Profile and Ratings)

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import cors from "cors"
 
 import game from  "./routes/game";
 import userRoutes from './routes/user';
@@ -13,10 +14,11 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/api/v1/game", game);
 app.use("/api/v1/users", userRoutes);
-app.use("/api/v1/userprofile", userProfileRoutes);
+app.use("/api/v1/profile", userProfileRoutes);
 app.use("/api/v1/ratings", userRatingsRoute);
 
 app.listen(port, () => {
