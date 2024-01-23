6986f8a8353aaa1fbc370a96528e44e86b125948
Rodrigo Gollo <rodrigogollo.nh@gmail.com>
2024-01-22 23:31:38 -0300

    feat: add navbar test and routing (bad)

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,7 @@
-import { Link, Route } from "wouter";
+import { Link, Route, Switch } from "wouter";
 import { useEffect, useState } from 'react'
 import GamePage from './GamePage';
+import Navbar from './Navbar';
 import './App.css'
 
 function App() {
@@ -22,21 +23,24 @@ function App() {
   //
   return (
     <>
-      <div>
-        <Link href="/users/1">
-          <a className='link'>Profile</a>
-        </Link>
-
-        <Route path="/about"> About Us </Route>
-        <Route path="/users/:name">{(params) => <div>Hello, {params.name}!</div>}</Route>
+      <>
         {
-          // users && 
-          // users.map(({id, username}: any) => {
-          //   return <p key={id}>{username}</p>
-          // })
+        // <Link href="/users/1">
+        //   <a className='link'>Profile</a>
+        // </Link>
         }
-        <GamePage IGDBgameId="212089" />
-      </div>
+          <Navbar />
+          <Route path="/">Home</Route>
+          <Route path="/about"> About Us </Route>
+          <Route path="/game/:name">{(params) => <GamePage IGDBgameId={params.name} />}</Route>
+          {
+            // users && 
+            // users.map(({id, username}: any) => {
+            //   return <p key={id}>{username}</p>
+            // })
+            // <GamePage IGDBgameId="212089" />
+          }
+      </>
     </>
   )
 }
---
+++ app/src/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <p>Navabar</p>
+    </nav>
+  )
+}
+
+export default Navbar;
